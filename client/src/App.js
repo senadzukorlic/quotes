5a0f80a9fccb1952ec7632e70074f13353f26bdf
Senad Zukorlic <zukorlic.senad01@gmail.com>
2024-08-02 19:52:43 +0200

    finally fixed problems

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useCallback } from "react"
 import { useAuth } from "./Context"
 import { QuoteCard } from "./Components/QuoteCard"
 import Filter from "./Components/Filter"
@@ -14,13 +14,7 @@ const App = () => {
   const [pageSize, setPageSize] = useState(1)
   const [quotesCount, setQuotesCount] = useState(0)
 
-  useEffect(() => {
-    if (token) {
-      fetchQuotes()
-    }
-  }, [tags, page, pageSize, token])
-
-  const fetchQuotes = async () => {
+  const fetchQuotes = useCallback(async () => {
     const queryParams = new URLSearchParams({
       tags: tags.join(","),
       page,
@@ -43,7 +37,13 @@ const App = () => {
     } catch (error) {
       console.error("Failed to fetch quotes", error)
     }
-  }
+  }, [tags, page, pageSize, token])
+
+  useEffect(() => {
+    if (token) {
+      fetchQuotes()
+    }
+  }, [fetchQuotes, token])
 
   return (
     <div className="parentDiv">
